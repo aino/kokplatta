9554604d208209c3c86760e6e58896ad457e8d24
Jakob Bergman <jakob@aino.se>
2014-03-27 08:48:24 +0100

    gulpfile.js had duplicate code

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+// Load plugins
 var path = require('path')
 var gulp = require('gulp')
 var gutil = require('gulp-util')
@@ -49,144 +50,6 @@ if ( ~~nodeversion.match(/\d+\.(\d+)/)[1] > 10 ) {
 } else if (shell.which('n')) {
   nodeversion = pjson.engines.node
   nodepath = shell.exec( 'n bin ' + nodeversion, { silent: true } ).output
-} else {
-  gutil.log('n not found. Please install it to enable node versioning.')
-  process.exit(1)
-}
-
-// Html
-gulp.task('html', function() {
-  var opts = {}
-  return gulp.src(config.src + 'html/*.html')
-    .pipe(htmltemplate( { 
-      debug: config.debug,
-      appname: pjson.name || 'Name your app'
-    }, {
-      'interpolate': /{{([\s\S]+?)}}/g
-    } ))
-    .pipe(htmlminify(opts))
-    .pipe(gulp.dest(config.public))
-})
-
-// Clean
-gulp.task('clean', function() {
-  return gulp.src([config.public + 'html', config.public + 'css', config.public + 'js', config.public + 'i'], {read: false})
-    .pipe(clean())
-})
-
-// Build lib css+js
-gulp.task('bundle:libs', function() {
-  
-  var scriptpaths = [config.src + 'js/_lib.js'].concat(config.bowerlibs.map(function(p) {
-    return './bower_components/' + p
-  }))
-  
-  var stylepaths = config.bowerstyles.map(function(p) {
-    return './bower_components/' + p
-  })
-
-  console.log(scriptpaths)
-
-  return es.concat(
-    gulp.src( scriptpaths )
-      .pipe(browserify( { require: config.libs } ))
-      .pipe(rename('libs.bundle.js'))
-      //.pipe(uglify())
-      .pipe(gulp.dest(config.public + 'js')),
-    gulp.src( stylepaths )
-      .pipe(concat('lib.bundle.css'))
-      .pipe(minifycss())
-      .pipe(gulp.dest(config.public + 'css'))
-  )
-})
-
-// Build app js
-gulp.task('bundle:appscripts', function() {
-  
-  return gulp.src( config.src + 'js/index.js', { read: false } )
-    .pipe( browserify({ 
-      transform: ['reactify', 'debowerify'], 
-      external: config.libs,
-      debug: true 
-    }) )
-    .pipe(rename('app.bundle.js'))
-    //.pipe(uglify())
-    .pipe(gulp.dest(config.public + 'js'))
-})
-
-// Build app css
-gulp.task('bundle:appstyles', function() {
-  return gulp.src(config.src + 'css/app.css')
-    //.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 2')) // <!-- production
-    .pipe(less({ sourceMap: true, silent: true }))  // <-- development
-    .pipe(rename('app.bundle.css'))
-    //.pipe(minifycss()) // <-- production
-    .pipe(gulp.dest(config.public + 'css'))
-})
-
-gulp.task('watch', function() {
-  gulp.watch(config.src + 'js/**/*.js', ['bundle:appscripts'])
-  gulp.watch(config.src + 'html/**/*.html', ['html'])
-  gulp.watch(config.src + 'css/**/*.css', ['bundle:appstyles'])
-  gulp.watch(config.src + 'i/**/*', ['images'])
-  gulp.watch('gulpfile.js', ['build:libs', 'build:app'])
-})
-
-gulp.task( 'supervisor', function() {
-  supervisor('server/app.js', {
-    args: [],
-    watch: [ 'node_modules', 'server', 'conf' ],
-    extensions: [ 'js', 'json' ],
-    exec: nodepath,
-    debug: true,
-    harmony: true,
-    noRestartOn: 'error',
-    quiet: false
-  } )
-} )
-
-gulp.task( 'build:app', ['bundle:appscripts', 'bundle:appstyles', 'html'], function () {
-  gutil.log( 'Build done' )
-})
-
-gulp.task( 'build:libs', ['bundle:libs'], function () {
-  gutil.log( 'Build done' )
-})
-
-gulp.task( 'default', ['build:app', 'watch', 'supervisor'], function () {
-  gutil.log( 'Running server on ' + config.port )
-})// Load plugins
-var path = require('path')
-var gulp = require('gulp')
-var gutil = require('gulp-util')
-var autoprefixer = require('gulp-autoprefixer')
-var minifycss = require('gulp-minify-css')
-var uglify = require('gulp-uglify')
-var rename = require('gulp-rename')
-var clean = require('gulp-clean')
-var concat = require('gulp-concat')
-var cache = require('gulp-cache')
-var supervisor = require('gulp-supervisor')
-var shell = require('shelljs')
-var htmltemplate = require('gulp-template')
-var htmlminify = require('gulp-minify-html')
-var browserify = require('gulp-browserify')
-var less = require('gulp-less')
-var es = require('event-stream')
-
-// Configs
-var pjson = require('./package.json')
-var config = require('./conf/config')
-
-// Check if installed version of node is enough. Need >=0.11
-// Else, check if n is installed and use that
-var nodeversion = process.version
-var nodepath = ''
-if ( ~~nodeversion.match(/\d+\.(\d+)/)[1] > 10 ) {
-  nodepath = shell.which('node')
-} else if (shell.which('n')) {
-  nodeversion = pjson.engines.node
-  nodepath = shell.exec( 'n bin ' + nodeversion ).output
 } else {
   gutil.log('n not found. Please install it to enable node versioning.')
   process.exit(1)
@@ -265,7 +128,7 @@ gulp.task('bundle:appstyles', function() {
 })
 
 gulp.task('watch', function() {
-  gulp.watch(config.src + 'js/**/*.js', ['bundle:appscripts', 'html'])
+  gulp.watch(config.src + 'js/**/*.js', ['bundle:appscripts'])
   gulp.watch(config.src + 'html/**/*.html', ['html'])
   gulp.watch(config.src + 'css/**/*.css', ['bundle:appstyles'])
   gulp.watch(config.src + 'i/**/*', ['images'])
@@ -285,7 +148,7 @@ gulp.task( 'supervisor', function() {
   } )
 } )
 
-gulp.task( 'build:app', ['html', 'bundle:appscripts', 'bundle:appstyles'], function () {
+gulp.task( 'build:app', ['bundle:appscripts', 'bundle:appstyles', 'html'], function () {
   gutil.log( 'Build done' )
 })
 
